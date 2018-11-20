7b6342194f5e9dcf8b11b883493e7a579320e871
Ole Ljosland <Ole.Johan.Ljosland@ehelse.no>
2018-11-20 23:05:48 +0100

    Litt endringer

--- OPPGAVER/DEL 6 - Konfigurasjon og avansert kompilering/oppgave1.ts
+++ OPPGAVER/DEL 6 - Konfigurasjon og avansert kompilering/oppgave1.ts
@@ -10,24 +10,26 @@
         "sourceMap": true,
         "outDir": "dist/"
 
-    4. Legg til hvilke filer/mapper som skal inkluderes i kompileringen:
+    4. Legg til ny mappe /src med en TypeScript-fil med noe innhold, feks. main.ts.
+
+    5. Legg til hvilke filer/mapper som skal inkluderes i kompileringen:
         "include": [
             "src/**\/*"
         ],
 
-    5. Legg til mapper som skal ekskluderes:
+    6. Legg til mapper som skal ekskluderes:
         "exclude": [
             "node_modules",
             "dist"
         ]
 
-    6. Kjør 'tsc'
-    7. Se genererte filer i /dist-mappa
+    7. Kjør 'tsc'
+    8. Se genererte filer i /dist-mappa
 
-    8. Endre module til 'amd' og legg til følgende i compilerOption:
+    9. Endre module til 'amd' og legg til følgende i compilerOption:
         "outFile": "./dist/build.js"
 
-    9. Kjør 'tsc' på nytt
-    10. Se generert build.js i /dist-mappa
-    
+    10. Kjør 'tsc' på nytt
+    11. Se generert build.js i /dist-mappa
+
     */

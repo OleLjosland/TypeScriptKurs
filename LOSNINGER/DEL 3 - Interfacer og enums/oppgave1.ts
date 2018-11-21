08792fe6816c8e9d5b09807399c01a42d282327d
Ole Ljosland <Ole.Johan.Ljosland@ehelse.no>
2018-11-21 15:25:05 +0100

    Løsning

--- LOSNINGER/DEL 3 - Interfacer og enums/oppgave1.ts
+++ LOSNINGER/DEL 3 - Interfacer og enums/oppgave1.ts
@@ -2,10 +2,22 @@
 
 // Erstatt any med en interface
 
-var objekt: any = {
+/*
+
+interface InterfaceTest {
+    value1: string;
+    value2: number;
+    value3: boolean;
+    value4: Array<number>,
+    value5: (parameter: string) => string;
+}
+
+var objekt: InterfaceTest = {
     value1: 'value',
     value2: 20,
     value3: true,
     value4: [1,2,3],
     value5: (parameter: string) => { return parameter.substring(0,1); }
 }
+
+*/

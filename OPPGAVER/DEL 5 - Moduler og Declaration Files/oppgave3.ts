70e28b056c5b55bce2bed75635064a2014527d54
Ole Ljosland <Ole.Johan.Ljosland@ehelse.no>
2018-11-21 13:16:31 +0100

    Oppgave

---
+++ OPPGAVER/DEL 5 - Moduler og Declaration Files/oppgave3.ts
@@ -0,0 +1,7 @@
+// MODULER OG DECLARATION FILES
+
+/*
+    1. Legg til referanse til en types i devDependencies i package.json, feks "@types/jquery": "^1.10.31",
+    2. Kjør 'npm install'
+    3. Se resultatet under mappen @types i node_modules 
+*/

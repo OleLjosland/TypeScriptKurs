b298d183a5f5a13e8ad3cda3fc60a6ca696976d3
Ole Ljosland <Ole.Johan.Ljosland@ehelse.no>
2018-11-21 15:38:46 +0100

    Endret litt på oppgave

--- OPPGAVER/DEL 1 - Installering og kompilering/oppgave1.ts
+++ OPPGAVER/DEL 1 - Installering og kompilering/oppgave1.ts
@@ -1,12 +1,20 @@
 // INSTALLERING OG KOMPILERING
 
 /* 
-    1. Kjør 'npm install'
-    2. Gå til denne mappen og kjør følgende: 
-        - tsc oppgave1.ts
-    3. Se resultatet i oppgave1.js
+    1. Kjør 'npm install -g typescript@latest'
+        Dersom du allerede har installert en eldre versjon av TypeScript er dette en fiks
+        - Kjør 'tsc -v' for å se versjon (den skal være 3.1.6)
+        - Kjør 'where tsc' for å se hvor TypeScript er installert
+        - Fjerne TypeScript-pathen i PATH i system variables, evt. slett mappen der gammel TypeScript er installert
+        - Installer TypeScript på nytt
 
-    4. Kjør 'node oppgave.js'
+    2. Kjør 'npm install'
+
+    4. Gå til denne mappen og kjør følgende: 
+        - tsc oppgave1.ts
+    5. Se resultatet i oppgave1.js
+
+    6. Kjør 'node oppgave.js'
 */
 
 function greeter(person: string): string {
